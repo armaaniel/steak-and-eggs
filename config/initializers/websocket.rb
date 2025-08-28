17d252e27eab6742d3194f72359b0ceaba07e789
armaaniel <ishmaan@hotmail.com>
2025-08-27 20:42:39 -0600

    we'll see

--- config/initializers/websocket.rb
+++ config/initializers/websocket.rb
@@ -1,12 +1,10 @@
 Rails.application.config.after_initialize do
   Thread.new do
-    retries = 0
-    
+        
     loop do
       client = Polygonio::Websocket::Client.new("stocks", ENV['API_KEY'], delayed: true)
       tickers = Ticker.pluck(:symbol)
       symbols = "A.#{tickers.join(',A.')}"
-      retries = 0
       
       client.subscribe(symbols) do |message|
         message.each do |data|
@@ -18,18 +16,12 @@ Rails.application.config.after_initialize do
         end
       rescue => e
         Sentry.capture_exception(e)
-      end
+      end      
       
     rescue Dry::Struct::Error, Dry::Types::SchemaError, Dry::Types::ConstraintError => e
-      retries += 1
-      if retries > 3
-        Rails.logger.info "Max connections reached - this worker will skip websocket (PID: #{Process.pid})"
-        Sentry.capture_exception(e)
-        break
-      else
-        Rails.logger.info "Max connections (#{retries}/3), retrying in 30s"
-        sleep 30
-      end
+      Rails.logger.info "Max connections reached - this worker will skip websocket (PID: #{Process.pid})"
+      Sentry.capture_exception(e)
+      break      
     rescue => e
       Sentry.capture_exception(e)
       sleep 10
