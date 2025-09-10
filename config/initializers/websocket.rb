babed244b9811f938d3f55e7343af2a10ba24d33
armaaniel <ishmaan@hotmail.com>
2025-09-10 16:44:32 -0600

    if can't delimit rescue

--- config/initializers/websocket.rb
+++ config/initializers/websocket.rb
@@ -1,29 +1,30 @@
 Rails.application.config.after_initialize do
   Thread.new do
-    
     if Rails.env.production?
       
-      client = Polygonio::Websocket::Client.new("stocks", ENV['API_KEY'], delayed: true)
-      tickers = Ticker.pluck(:symbol)
-      symbols = "A.#{tickers.join(',A.')}"
+      begin
+        client = Polygonio::Websocket::Client.new("stocks", ENV['API_KEY'], delayed: true)
+        tickers = Ticker.pluck(:symbol)
+        symbols = "A.#{tickers.join(',A.')}"
         
-      client.subscribe(symbols) do |message|
-        message.each do |data|
-          RedisService.safe_setex("price:#{data.sym}", 6.days.to_i, data.c)
-          RedisService.safe_setex("open:#{data.sym}", 6.days.to_i, data.op)
-          ActionCable.server.broadcast("price_channel:#{data.sym}", data.c)
+        client.subscribe(symbols) do |message|
+          message.each do |data|
+            RedisService.safe_setex("price:#{data.sym}", 6.days.to_i, data.c)
+            RedisService.safe_setex("open:#{data.sym}", 6.days.to_i, data.op)
+            ActionCable.server.broadcast("price_channel:#{data.sym}", data.c)
+          rescue => e
+            Sentry.capture_exception(e)
+          end
         rescue => e
           Sentry.capture_exception(e)
-        end
+        end      
+    
+      rescue Dry::Struct::Error, Dry::Types::SchemaError, Dry::Types::ConstraintError => e
+        Rails.logger.info "Max connections reached - this worker will skip websocket (PID: #{Process.pid})"
+    
       rescue => e
         Sentry.capture_exception(e)
-      end      
-    
-    rescue Dry::Struct::Error, Dry::Types::SchemaError, Dry::Types::ConstraintError => e
-      Rails.logger.info "Max connections reached - this worker will skip websocket (PID: #{Process.pid})"
-    
-    rescue => e
-      Sentry.capture_exception(e)
+      end
     end
   end
 end
