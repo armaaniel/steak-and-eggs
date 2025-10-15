4e4b873342c0c086e14a1084facdf5aaf2ec289a
armaaniel <ishmaan@hotmail.com>
2025-10-15 12:42:02 -0600

    test forced websocket disconnect & reconnect

--- config/initializers/websocket.rb
+++ config/initializers/websocket.rb
@@ -1,44 +1,54 @@
 Rails.application.config.after_initialize do
-  Thread.new do
-    if Rails.env.production?
-  
+  if Rails.env.production?
+    # Start a monitoring thread that will periodically restart the WebSocket
+    Thread.new do
       loop do
-        10.times do
-          puts("trying to connect to ws, #{Time.current}")
-        end
-        client = Polygonio::Websocket::Client.new("stocks", ENV['API_KEY'], delayed: true)
-        tickers = Ticker.pluck(:symbol)
-        symbols = "A.#{tickers.join(',A.')}"
-        
-        client.subscribe(symbols) do |message|
-          message.each do |data|
-            RedisService.safe_setex("price:#{data.sym}", 6.days.to_i, data.c)
-            RedisService.safe_setex("open:#{data.sym}", 6.days.to_i, data.op)
-            ActionCable.server.broadcast("price_channel:#{data.sym}", data.c)
+        # Create a thread for the WebSocket connection
+        websocket_thread = Thread.new do
+          begin
+            puts("Starting new WebSocket connection at #{Time.current}")
+            client = Polygonio::Websocket::Client.new("stocks", ENV['API_KEY'], delayed: true)
+            tickers = Ticker.pluck(:symbol)
+            symbols = "A.#{tickers.join(',A.')}"
+            
+            client.subscribe(symbols) do |message|
+              message.each do |data|
+                RedisService.safe_setex("price:#{data.sym}", 6.days.to_i, data.c)
+                RedisService.safe_setex("open:#{data.sym}", 6.days.to_i, data.op)
+                ActionCable.server.broadcast("price_channel:#{data.sym}", data.c)
+              rescue => e
+                Sentry.capture_exception(e)
+              end
+            rescue => e
+              Sentry.capture_exception(e)
+            end
+            
+            # If we ever get here naturally (which we likely won't), log it
+            puts("WebSocket connection ended naturally at #{Time.current}")
           rescue => e
             Sentry.capture_exception(e)
+            puts("WebSocket error: #{e.message} at #{Time.current}")
           end
-        rescue => e
-          Sentry.capture_exception(e)
         end
         
-        Sentry.capture_message("Websocket connection ended, #{Time.current}")
-        25.times do
-          puts("!!!@@@@@!!!&&& WEBSOCKET DIED &&& $$$$ && && && & & & @@@@ #{Time.current}")
-        end 
+        # Set maximum lifetime for this connection thread
+        max_lifetime = 15.minutes
         
-        sleep(900)
+        # Wait for that duration
+        sleep(max_lifetime)
         
-        10.times do
-          puts("trying again at #{Time.current}")
+        # After the timeout, kill the WebSocket thread if it's still alive
+        if websocket_thread.alive?
+          puts("Maximum lifetime reached. Killing WebSocket thread at #{Time.current}")
+          websocket_thread.kill
+          sleep(30) # Give it a moment to clean up
         end
-      
-      rescue => e
-        Sentry.capture_exception(e)
-        10.times do 
-          puts("I rescued #{e.class} - #{e.message} - #{Time.current}")
-        end
-        break
+        
+        # Log this cycle completed
+        puts("WebSocket cycle completed at #{Time.current}, restarting...")
+        
+        # Optional: short delay before starting a new connection
+        sleep(30)
       end
     end
   end
