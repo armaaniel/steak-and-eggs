41807e145c4aa95aa8a2318f874ebcebc38b24b4
armaaniel <ishmaan@hotmail.com>
2026-03-07 14:10:31 -0700

    remove mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,7 +2,5 @@
 
 module Types
   class MutationType < Types::BaseObject
-    field(:create_user, mutation: Mutations::CreateUser)
-    field(:modify_user, mutation: Mutations::ModifyUser)
   end
 end
