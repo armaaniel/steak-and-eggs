0b4279bfce8161e851cad25176ba32bac79f2fa1
armaaniel <ishmaan@hotmail.com>
2026-04-01 00:48:40 -0600

    fix cd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,6 +21,7 @@ class HomeController < ApiController
 
   def get_portfolio_data
     result = PositionService.get_aum(user_id:@current_user.id, balance:@current_user.balance)
+    Rails.logger.info "Deploy successful: #{Time.current}"
     render(json: result)
 
   rescue => e
